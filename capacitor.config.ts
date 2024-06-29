5c0d90097a225034588d381e57db79972f9ca816
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-29 09:11:27 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rose-swap',
+  webDir: 'www'
+};
+
+export default config;
