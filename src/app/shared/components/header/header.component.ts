feae351d1e78dfe5a721780cd20554b84b8286a7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-01 10:51:23 +0200

    add the header to AppComponent

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  imports: [
+    IonicModule
+  ],
+  standalone: true
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
 
   constructor() { }
 
