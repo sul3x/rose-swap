3493f8a0547f8fdcb46f67010295a59b13c0c3bb
afcasco <7701272+afcasco@users.noreply.github.com>
2024-07-07 12:25:21 +0200

    remove duplicated login route2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,6 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/public/login/login.page').then(m => m.LoginPage)
-  },
-  {
-    path: 'login',
     loadComponent: () => import('./pages/public/login/login.page').then( m => m.LoginPage)
   },
 ];
