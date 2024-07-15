9ed0084d7917cb58faf72b73e7c89ae5d6679def
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-15 19:46:20 +0200

    implement initializeAlertInput() and create the view

--- src/app/pages/private/tab1/tab1.page.ts
+++ src/app/pages/private/tab1/tab1.page.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRow,
+  IonList,
+  IonItem,
+  IonLabel, IonButton, IonAlert
+} from '@ionic/angular/standalone';
+import { MyRoseGardenService } from "../../../services/my-rose-garden.service";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRow, IonList, IonItem, IonLabel, IonButton, IonAlert],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(public myRoseGardenService: MyRoseGardenService) {}
+
+  public alertButtons = [{}];
+  public alertInputs = [{}];
+
+  initializeAlertInput() {
+    this.alertButtons = ['OK'];
+    this.alertButtons = [
+      {
+        type: 'button',
+        text: 'Photo',
+        handler: () => {
+          this.myRoseGardenService.addNewPhotoRose();
+        }
+      },
+      {
+        text: 'OK:)'
+      },
+    ];
+    this.alertInputs = [
+      {
+        placeholder: 'Name',
+        attributes: {
+          maxlength: 8,
+        },
+      },
+      {
+        type: 'number',
+        placeholder: 'Fragrance (1 to 10)',
+        min: 1,
+        max: 10,
+      },
+      {
+        type: 'textarea',
+        placeholder: 'A little more about the rose',
+      },
+    ];
+  }
+
 }
