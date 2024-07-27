a40b455884ac89361aab7f73da3985d17d42e329
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-28 01:30:42 +0200

    update validate inputs

--- src/app/pages/private/tab1/tab1.page.ts
+++ src/app/pages/private/tab1/tab1.page.ts
@@ -24,24 +24,22 @@ import {
   IonFab,
   IonFabButton,
   IonIcon,
-  IonThumbnail
+  IonThumbnail,
+  AlertController
 } from '@ionic/angular/standalone';
 import { MyRoseGardenService } from "../../../services/my-rose-garden.service";
 import { IRose } from "../../../model/interfaces";
 import { addIcons } from "ionicons";
-import { AlertController } from "@ionic/angular";
 import { addOutline, trashOutline, pencilOutline } from "ionicons/icons";
 import { Timestamp } from "@angular/fire/firestore";
 import { AuthService } from "../../../services/auth.service";
-import {HeaderComponent} from "../../../shared/components/header/header.component";
-import {TabsPage} from "../tabs/tabs.page";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRow, IonList, IonItem, IonLabel, IonButton, IonAlert, IonInput, IonImg, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonModal, IonButtons, IonFab, IonFabButton, IonIcon, IonThumbnail, HeaderComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRow, IonList, IonItem, IonLabel, IonButton, IonAlert, IonInput, IonImg, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonModal, IonButtons, IonFab, IonFabButton, IonIcon, IonThumbnail],
 })
 export class Tab1Page implements OnInit {
 
@@ -52,7 +50,8 @@ export class Tab1Page implements OnInit {
     private platform: Platform,
     private myRoseGardenService: MyRoseGardenService,
     private alertCtrl: AlertController,
-    private authService: AuthService
+    private authService: AuthService,
+    private alertController: AlertController
   ) {
     this.platform = platform;
   }
@@ -84,40 +83,56 @@ export class Tab1Page implements OnInit {
         inputs: [
           {
             type: 'text',
-            placeholder: 'Name'
+            placeholder: 'Name',
+            attributes: {
+              required: true,
+              minlength: 2,
+              maxlength: 12
+            }
           },
           {
             type: 'number',
             placeholder: 'Cuttings',
+            attributes: {
+              required: true,
+              max: 100
+            }
           },
           {
             type: 'number',
-            placeholder: 'Fragrance (1 to 10)',
-            min: 1,
-            max: 10,
+            placeholder: 'Fragrance (0 to 10)',
+            attributes: {
+              max: 10
+            }
           },
           {
             type: 'textarea',
             placeholder: 'Description',
+            attributes: {
+              required: true,
+              maxlength: 30
+            }
           }
         ],
         buttons: [
           {
             text: 'Add',
             handler: (roseData: any): void => {
-              if (userId) {
-                const newRose: IRose = {
-                  name: roseData[0],
-                  intensityFragrance: roseData[1],
-                  cuttings: roseData[2],
-                  moreInfo: roseData[3],
-                  addedAt: Timestamp.now(),
-                  userId: userId
-                };
-                this.myRoseGardenService.addRose(newRose);
-                console.log('rose userId: ', newRose.userId)
-              } else {
-                console.error('No user ID found');
+              if (!this.validateInputs(roseData)) {
+                if (userId) {
+                  const newRose: IRose = {
+                    name: roseData[0],
+                    intensityFragrance: roseData[1],
+                    cuttings: roseData[2],
+                    moreInfo: roseData[3],
+                    addedAt: Timestamp.now(),
+                    userId: userId
+                  };
+                  this.myRoseGardenService.addRose(newRose);
+                  console.log('rose userId: ', newRose.userId)
+                } else {
+                  console.error('No user ID found');
+                }
               }
             }
           },
@@ -131,6 +146,84 @@ export class Tab1Page implements OnInit {
       console.log('User ID at addRose:', userId);
     });
 
+  }
+
+  async validateInputs(roseData: IRose) {
+
+    if (!roseData.name || roseData.name.length < 2 || roseData.name.length > 12) {
+      await this.alertRoseDataName();
+      return false;
+    }
+    if (!roseData.cuttings || roseData.cuttings < 0 || roseData.cuttings > 100) {
+      await this.alertRoseDataCuttings();
+      return false;
+    }
+    if (!roseData.intensityFragrance || roseData.intensityFragrance < 0 || roseData.intensityFragrance > 10) {
+      await this.alertRoseDataFragrance();
+      return false;
+    }
+    if (!roseData.moreInfo || roseData.moreInfo.length > 30) {
+      await this.alertRoseDataMoreInfo();
+      return false;
+    }
+
+    return true;
+  }
+
+  async alertRoseDataName() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Please insert rose name (2 to 12 characters).',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          alert.dismiss()
+        }
+      }]
+    })
+    await alert.present();
+  }
+
+  async alertRoseDataFragrance() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Please insert intensity of fragrance (0 to 10)',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          alert.dismiss()
+        }
+      }]
+    })
+    await alert.present();
+  }
+
+  async alertRoseDataCuttings() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Please insert number of cuttings (0 to 100)',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          alert.dismiss()
+        }
+      }]
+    })
+    await alert.present();
+  }
+
+  async alertRoseDataMoreInfo() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Please insert more info (max 30 characters)',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          alert.dismiss();
+        }
+      }]
+    });
+    await alert.present();
   }
 
   editRose(rose: IRose) {
