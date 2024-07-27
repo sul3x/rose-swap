5a3080fc83a30cf6f56a55e8b93bcfb93f352ca4
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-27 11:07:17 +0200

    icons dark-mode

--- src/app/pages/private/tabs/tabs.page.ts
+++ src/app/pages/private/tabs/tabs.page.ts
@@ -1,4 +1,4 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import {Component, EnvironmentInjector, inject, OnInit} from '@angular/core';
 import {IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonContent} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import {ellipse, heartCircleOutline, square} from 'ionicons/icons';
@@ -11,15 +11,26 @@ import {HeaderComponent} from "../../../shared/components/header/header.componen
   standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, HeaderComponent, IonContent],
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
   public environmentInjector = inject(EnvironmentInjector);
+  isDarkMode: boolean = false;
 
   constructor() {
     addIcons(
       {
-        "my-garden" : "../../assets/icon/my-garden.svg",
+        'my-garden' : '../../assets/icon/my-garden.svg',
         'other-gardens': '../../assets/icon/other-gardens.svg',
+        'my-garden-dark' : '../../assets/icon/my-garden-dark.svg',
+        'other-gardens-dark': '../../assets/icon/other-gardens-dark.svg',
         heartCircleOutline
       });
+  }
+
+  ngOnInit() {
+    this.isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      this.isDarkMode = event.matches;
+    });
   }
 }
