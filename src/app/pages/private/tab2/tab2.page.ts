3434c867c7fae60b7d26d5eb16aa224dcfa9872d
Alex <afernandezca@opentrends.net>
2024-07-27 11:20:58 +0200

    add capacitor google maps plugin

--- src/app/pages/private/tab2/tab2.page.ts
+++ src/app/pages/private/tab2/tab2.page.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Component, ElementRef, ViewChild, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from '../../../../environments/environment';
+import {IonContent, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class Tab2Page {
+export class Tab2Page implements AfterViewInit {
+  @ViewChild('map') mapRef: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
 
   constructor() {}
 
+  async ngAfterViewInit() {
+    await this.createMap();
+  }
+
+  async createMap() {
+    console.log('Creating map...');
+    try {
+      this.newMap = await GoogleMap.create({
+        id: 'my-cool-map',
+        element: this.mapRef.nativeElement,
+        apiKey: environment.apiKey,
+        config: {
+          center: {
+            lat: 33.6,
+            lng: -117.9,
+          },
+          zoom: 8,
+        },
+      });
+      console.log('Map created successfully');
+    } catch (error) {
+      console.error('Error creating map:', error);
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyATpJCxQvb_LF2GujQRf62iq7cELpicfss"
 };
 
 /*
