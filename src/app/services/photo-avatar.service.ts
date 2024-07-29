8600c68316b9ab04708a5f0691cb3a5c623366a9
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-29 09:48:31 +0200

    get user id

--- src/app/services/photo-avatar.service.ts
+++ src/app/services/photo-avatar.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Auth} from '@angular/fire/auth';
-import {Firestore} from '@angular/fire/firestore';
+import {doc, docData, Firestore} from '@angular/fire/firestore';
 import {Storage} from '@angular/fire/storage';
 import {Photo} from '@capacitor/camera';
 
@@ -14,4 +14,10 @@ export class PhotoAvatarService {
     private firestore: Firestore,
     private storage: Storage
   ) { }
+
+  getUserProfileData() {
+    const user = this.auth.currentUser;
+    const userDocRef = doc(this.firestore, `userprofile/${user.uid}`);
+    return docData(userDocRef);
+  }
 }
