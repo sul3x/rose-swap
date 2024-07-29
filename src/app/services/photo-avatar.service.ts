2eb8af813b144695f06333945e69a658170edb82
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-29 10:04:28 +0200

    i want to know the imageUrl

--- src/app/services/photo-avatar.service.ts
+++ src/app/services/photo-avatar.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Auth} from '@angular/fire/auth';
 import {doc, docData, Firestore} from '@angular/fire/firestore';
-import {ref, Storage, uploadString} from '@angular/fire/storage';
+import {getDownloadURL, ref, Storage, uploadString} from '@angular/fire/storage';
 import {Photo} from '@capacitor/camera';
 
 @Injectable({
@@ -28,6 +28,9 @@ export class PhotoAvatarService {
 
     try {
       await uploadString(storageRef, cameraFile.base64String, "base64");
+
+      const imageUrl = await getDownloadURL(storageRef);
+      console.log('image url: ', imageUrl);
     } catch (e) {
 
     }
